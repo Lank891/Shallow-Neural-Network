76ca7496b9fa3a9c63c8818e7f3a5397e112d0e3
Andrzej Litkowski <h99_10@o2.pl>
2022-04-08 21:09:00 +0200

    Update OutputSetWriter.cs

--- Shallow Neural Network/Classification/OutputSetWriter.cs
+++ Shallow Neural Network/Classification/OutputSetWriter.cs
@@ -9,27 +9,27 @@ namespace Classification
 {
     public interface IOutputSetWriter
     {
-        public void Write(string path, string allNeuronsPath, List<List<double>> inputSet,List<List<double>> outputSet);
+        public void Write(string path, string allNeuronsPath, List<List<double>> inputSet, List<List<double>> outputSet);
     }
 
     public class OutputSetWriter : IOutputSetWriter
     {
-        public void Write(string path,string allNeuronsPath, List<List<double>> inputSet, List<List<double>> outputSet)
+        public void Write(string path, string allNeuronsPath, List<List<double>> inputSet, List<List<double>> outputSet)
         {
-            List<string> vs = new List<string>();
+            List<string> outputLines = new List<string>();
             for(int i = 0; i < inputSet.Count; i++)
             {
-                string classID = OneHot(outputSet[i]);
-                vs.Add(string.Join("\t", inputSet[i])+"\t"+classID);
+                int classIndex = PredictedClassIndex(outputSet[i]);
+                outputLines.Add(string.Join("\t", inputSet[i]) + "\t" + classIndex.ToString());
 
             }
-            File.WriteAllLines(path, vs); /*outputs ids of classes found for the given inputs*/
-            File.WriteAllLines(allNeuronsPath, outputSet.Select(x => string.Join("\t", x))); /*outputs values returned by each output neuron*/
+            File.WriteAllLines(path, outputLines); /* Outputs ids of classes found for the given inputs */
+            File.WriteAllLines(allNeuronsPath, outputSet.Select(x => string.Join("\t", x))); /* Outputs values returned by each output neuron */
         }
 
-        public static string OneHot(List<double> listOfOutputsForOneInput)
+        public static int PredictedClassIndex(List<double> listOfOutputsForOneInput)
         {
-            return (listOfOutputsForOneInput.IndexOf(listOfOutputsForOneInput.Max())+1).ToString();
+            return listOfOutputsForOneInput.IndexOf(listOfOutputsForOneInput.Max()) + 1;
         }
     }
 }
